d20134c7593cdba5ad8ff497d1c5043bbdf55fdd
ismail2009 <somaaa2009@live.com>
2018-01-15 12:31:55 +0200

    remove commented code

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 const home = require('./home');
-// const about = require('about');
-// const contact = require('contact');
-// const levels = require('levels');
-// const game = require('game');
 
 router.get('/', home.get);
 module.exports = router;
