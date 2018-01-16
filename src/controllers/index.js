037bb5c43c174e79f7a56870693d9c3fe45f5c2d
ismail2009 <somaaa2009@live.com>
2018-01-16 12:40:31 +0200

    add new router for game page #11

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const home = require('./home');
+const game = require('./game');
 
 router.get('/', home.get);
+router.get('/game', game.get);
 module.exports = router;
