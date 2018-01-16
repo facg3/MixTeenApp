526c0b85b867ae94406c6189b0e325e4dfa6f7c8
NadiaKhatib <nadosh180@hotmail.com>
2018-01-16 14:19:46 +0200

    I add router levels #7

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const home = require('./home');
+const levels = require('./levels');
 
 router.get('/', home.get);
+router.get('/levels', levels.get);
 module.exports = router;
