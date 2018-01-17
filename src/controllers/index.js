fb4b720e48db7a488877be7e18faf183bcc49f9d
MarwaBj <negmah95@hotmail.com>
2018-01-17 09:36:31 +0200

    modifey about files

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,4 +4,7 @@ const router = express.Router();
 const home = require('./home');
 
 router.get('/', home.get);
+const contact = require('./contact');
+
+router.get('/contact', contact.get);
 module.exports = router;
