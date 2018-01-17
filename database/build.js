45926031b66098a94854973dad00759a3c94f4f7
NadiaKhatib <nadosh180@hotmail.com>
2018-01-17 16:06:16 +0200

    edit all files

--- database/build.js
+++ database/build.js
@@ -1,13 +1,13 @@
-
 const connect = require('./dbConnection');
 const fs = require('fs');
 const path = require('path');
 
-const sql = fs.readFileSync(path.join(__dirname, '/build.sql'));
-connect.query(sql, (err, res) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(res);
-  }
+fs.readFileSync(path.join(__dirname, '/build.sql'), (error, sql) => {
+  connect.query(sql, (err, res) => {
+    if (err) {
+      throw new Error('There is an Error in SQL statment $ {err}');
+    } else {
+      console.log(res);// eslint-disable-line
+    }
+  });
 });
---
+++ database/build.sql
@@ -0,0 +1,25 @@
+BEGIN;
+
+
+CREATE TABLE IF NOT EXISTS levels(
+  level_id SERIAL PRIMARY KEY,
+  level_name VARCHAR(25) NOT NULL,
+  instruction VARCHAR(1000) NOT NULL
+
+);
+
+INSERT INTO levels (level_name, instruction)
+  VALUES ('Level-1', 'Instruction Level One'),
+         ('Level-2', 'Instruction level Two'),
+         ('Level-3', 'Instruction level Three');
+
+CREATE TABLE IF NOT EXISTS comment(
+           id SERIAL PRIMARY KEY,
+           username VARCHAR(25) NOT NULL,
+           email VARCHAR(40) NOT NULL,
+           message VARCHAR(1000) NOT NULL
+         );
+  INSERT INTO comment (username, email,message)
+           VALUES ('Ismail', 'ismail12@gmail.com','website its very good');
+
+COMMIT;
