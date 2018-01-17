320a5bb9acceae59ed59128f33df6ddfc0e31d94
NadiaKhatib <nadosh180@hotmail.com>
2018-01-17 13:25:54 +0200

    edit database

--- database/queries/instruction.js
+++ database/queries/instruction.js
@@ -1,15 +1,15 @@
 const connect = require('../dbConnection');
+
 const getInstruction = (req, res, cb) => {
-const sql = {
-   text: 'delete from members where level_id=$1',
-   values: [JSON.parse(req.body).level_id],
-};
-const getInstruction = (cb) => {
-  connect.query(sql, (err, res) => {
+  const sql = {
+    text: 'delete from members where level_id=$1',
+    values: [JSON.parse(req.body).level_id],
+  };
+  connect.query(sql, (err, result) => {
     if (err) {
       cb(err);
     } else {
-      cb(null, res.rows);
+      cb(null, result.rows);
     }
   });
 };
