0f34ee48e947ef208fe1cd6642078f375672a457
ismail2009 <somaaa2009@live.com>
2018-01-17 11:00:15 +0200

    simplify router.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,6 +4,5 @@ const router = express.Router();
 const home = require('./home');
 const game = require('./game');
 
-router.get('/', home.get);
-router.get('/game', game.get);
-module.exports = router;
+module.exports = router.get('/', home.get)
+  .get('/game', game.get);
