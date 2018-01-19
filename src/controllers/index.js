1415d77032b7110f837df299db2c8bf70582b70a
MarwaBj <negmah95@hotmail.com>
2018-01-19 20:14:42 +0200

    congrate pages

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 const home = require('./home');
 const levels = require('./levels');
+const congrate = require('./congrate');
 
-router.get('/', home.get);
-router.get('/levels', levels.get);
-module.exports = router;
+module.exports = router
+  .get('/', home.get)
+  .get('/levels', levels.get)
+  .get('/congrate', congrate.get);
