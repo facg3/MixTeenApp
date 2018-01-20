2fbca660293c0c537d42d3c7920b0053994e51b7
MarwaBj <negmah95@hotmail.com>
2018-01-21 00:22:06 +0200

    some modified

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,9 +5,13 @@ const home = require('./home');
 const levels = require('./levels');
 const congrate = require('./congrate');
 const again = require('./again');
+const about = require('./about');
+const contact = require('./contact');
 
 module.exports = router
   .get('/', home.get)
   .get('/levels', levels.get)
   .get('/congrate', congrate.get)
-  .get('/again', again.get);
+  .get('/again', again.get)
+  .get('/about', about.get)
+  .get('/contact', contact.get);
