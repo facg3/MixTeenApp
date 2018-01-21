625c748a702a69c6d789bd986b81654f0e40c5b3
ismail2009 <somaaa2009@live.com>
2018-01-21 17:42:10 +0200

    remove levels roouter from routers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const home = require('./home');
-const levels = require('./levels');
 const game = require('./game');
 
 router.get('/', home.get)
-  .get('/levels', levels.get)
   .get('/game', game.get);
 module.exports = router;
