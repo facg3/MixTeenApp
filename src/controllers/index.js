cd487b440d8f0c2e8dd373b13c45e58359835986
ismail2009 <somaaa2009@live.com>
2018-01-21 17:27:12 +0200

    modifiy routeres

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 const home = require('./home');
 const levels = require('./levels');
+const game = require('./game');
 
-router.get('/', home.get);
-router.get('/levels', levels.get);
+router.get('/', home.get)
+  .get('/levels', levels.get)
+  .get('/game', game.get);
 module.exports = router;
