5ecbf1ff98ff4ba3925ff4d105378e1fac56e6cd
ismail2009 <somaaa2009@live.com>
2018-01-21 17:35:15 +0200

    write fonctionality of home #5

---
+++ public/js/homeDom.js
@@ -0,0 +1,5 @@
+
+const navButton = document.querySelector('.burger-btn'); // eslint-disable-line
+navButton.addEventListener('click', () => {
+  document.querySelector('.list-nav').classList.toggle('active'); // eslint-disable-line
+});
