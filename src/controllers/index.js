3511221c626009f531ccde8b1e6d8405a7b36d6c
MarwaBj <negmah95@hotmail.com>
2018-01-22 11:46:03 +0200

    add congrate page files

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const home = require('./home');
 const game = require('./game');
 const tryAgain = require('./tryAgain');
+const congrate = require('./congrate');
 
 module.exports = router
   .get('/', home.get)
   .get('/game', game.get)
-  .get('/tryAgain', tryAgain.get);
+  .get('/tryAgain', tryAgain.get)
+  .get('/congrate', congrate.get);
