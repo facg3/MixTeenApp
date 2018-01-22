d9006d5ca1aa6b6e4ba152f7048e8a80abfd4425
MarwaBj <negmah95@hotmail.com>
2018-01-22 10:58:52 +0200

    modified contact page files

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 const home = require('./home');
 const game = require('./game');
+const contact = require('./contact');
 
-router.get('/', home.get)
-  .get('/game', game.get);
-module.exports = router;
+module.exports = router
+  .get('/', home.get)
+  .get('/game', game.get)
+  .get('/contact', contact.get);
