a861124add724ca01115f314e2a0ff48226f9ef6
ismail2009 <somaaa2009@live.com>
2018-01-23 18:35:01 +0200

    add signup and signin and logout routers #62

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,17 @@ const express = require('express');
 const router = express.Router();
 const home = require('./home');
 const game = require('./game');
+const signin = require('./signin');
+const signup = require('./signup');
+const logout = require('./logout');
 
-router.get('/', home.get)
-  .get('/game', game.get);
+
+router
+  .post('/signup', signup.post)
+  .get('/', home.get)
+  .get('/game', game.get)
+  .get('/signin', signin.get)
+  .post('/signin', signin.post)
+  .get('/signup', signup.get)
+  .get('/logout', logout.get);
 module.exports = router;
