746c80d7d1c984f81811b650c094232f80d94c29
ismail2009 <somaaa2009@live.com>
2018-01-23 18:31:15 +0200

    built signin page and style it and add function for page for authontication #62

---
+++ src/controllers/signin.js
@@ -0,0 +1,29 @@
+const user = require('../../database/queries/user');
+const bcrypto = require('../script/bcrypto');
+const jwt = require('jsonwebtoken');
+require('env2')('config.env');
+
+exports.get = (req, res) => {
+  res.render('signin', {
+    loginstyle: true,
+    login: false,
+    activePage: {
+      game: true,
+    },
+  });
+};
+exports.post = (req, res) => {
+  const { username, password } = req.body;
+  user.checkUser(username).then((data) => {
+    bcrypto.compPassword(password, data.rows[0].password)
+      .then((result) => {
+        if (result) {
+          const userData = { id: data.rows[0].id, username: data.rows[0].username };
+          const token = jwt.sign(userData, process.env.TOKEN_SECRET);
+          res.cookie('token', token);
+          res.json({ success: true, username: userData.username, login: true });
+        } else res.json({ massege: 'password is wrong' });
+      })
+      .catch((error) => { if (error)res.render('signin'); });
+  }).catch((err) => { if (err) res.json({ message: 'username is not valid' }); });
+};
