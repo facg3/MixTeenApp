fcafaa569fbfcabb8db3b2ff81f218fe8b285b54
ismail2009 <somaaa2009@live.com>
2018-01-23 18:22:38 +0200

    modifiy game page after adding user feature 62

--- src/controllers/game.js
+++ src/controllers/game.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken');
+require('env2')('config.env');
 
 exports.get = (req, res) => {
-  res.render('game', {
-    game: true,
-  });
+  const cookie = req.cookies.token || '';
+  if (cookie) {
+    jwt.verify(cookie, process.env.TOKEN_SECRET, (error, d) => {
+      if (error) {
+        res.redirect('/');
+      } else {
+        res.status(200).render('game', { game: true, login: true, username: d.username });
+      }
+    });
+  } else res.redirect('/');
 };
