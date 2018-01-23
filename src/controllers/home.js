bb0846c9607fb978b1b8c65e4700293c71619494
ismail2009 <somaaa2009@live.com>
2018-01-23 18:26:32 +0200

    modifies home page after add user feature 62#

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,15 @@
+const jwt = require('jsonwebtoken');
+require('env2')('config.env');
+
 exports.get = (req, res) => {
-  res.render('home', { home: true });
+  const cookie = req.cookies.token || '';
+  if (cookie) {
+    jwt.verify(cookie, process.env.TOKEN_SECRET, (error, d) => {
+      if (error) {
+        res.status(200).render('home', { home: true });
+      } else {
+        res.status(200).render('home', { home: true, login: true, username: d.username });
+      }
+    });
+  } else res.status(200).render('home', { home: true });
 };
