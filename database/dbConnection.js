d3bf75a7e79b9250e3d23b09c6baf492eef2dcec
ismail2009 <somaaa2009@live.com>
2018-01-23 18:13:32 +0200

    rename file name to be sensitive

--- database/dbConnection.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-require('env2')('config.env');
-
-if (!process.env.DB_URL) {
-  throw new Error('DATABASE_URL  is undefined. ');
-}
-module.exports = new Pool({
-  connectionString: process.env.DB_URL,
-});
