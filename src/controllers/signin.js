30f5e72a191bf245e51443e5beada53064c83eef
ismail2009 <somaaa2009@live.com>
2018-01-24 11:10:51 +0200

    add handle to user signin

--- src/controllers/signin.js
+++ src/controllers/signin.js
@@ -15,15 +15,19 @@ exports.get = (req, res) => {
 exports.post = (req, res) => {
   const { username, password } = req.body;
   user.checkUser(username).then((data) => {
-    bcrypto.compPassword(password, data.rows[0].password)
-      .then((result) => {
-        if (result) {
-          const userData = { id: data.rows[0].id, username: data.rows[0].username };
-          const token = jwt.sign(userData, process.env.TOKEN_SECRET);
-          res.cookie('token', token);
-          res.json({ success: true, username: userData.username, login: true });
-        } else res.json({ massege: 'password is wrong' });
-      })
-      .catch((error) => { if (error)res.render('signin'); });
+    if (data.rowCount !== 0) {
+      bcrypto.compPassword(password, data.rows[0].password)
+        .then((result) => {
+          if (result) {
+            const userData = { id: data.rows[0].id, username: data.rows[0].username };
+            const token = jwt.sign(userData, process.env.TOKEN_SECRET);
+            res.cookie('token', token);
+            res.json({ success: true, username: userData.username, login: true });
+          } else res.json({ massege: 'password is wrong' });
+        })
+        .catch((error) => { if (error)res.render('signin'); });
+    } else {
+      res.json({ message: 'user is not found' });
+    }
   }).catch((err) => { if (err) res.json({ message: 'username is not valid' }); });
 };
